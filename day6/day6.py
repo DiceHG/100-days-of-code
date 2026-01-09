0f880883e74b0e803b476d1b064d8a59e85081e8
Eduardo Higino <eduardo.h.garcia2002@gmail.com>
2026-01-09 18:32:07 -0300

    finished day 6 project

---
+++ day6/day6.py
@@ -0,0 +1,14 @@
+# from library import turn_right
+
+# while not wall_in_front():
+#     move()
+# turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
